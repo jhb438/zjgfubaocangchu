6c79dedb77b9e9374ce76d69f8b2b1008b4a30ea
hero12345679 <1039323578@qq.com>
2020-04-20 18:16:24 +0800

    统计

--- daima/zjgfbcc/src/main/java/com/basic/zjgfbcc/controller/CommonController.java
+++ daima/zjgfbcc/src/main/java/com/basic/zjgfbcc/controller/CommonController.java
@@ -66,131 +66,46 @@ public class CommonController extends BaseController {
     @ResponseBody
     @RequestMapping(value = "/StatisticsBG", produces = "application/json;charset=utf-8", method = RequestMethod.POST)
     public R StatisticsBG() {
-    	
-    	List<Map<String,String>> mapList = fbDooreventsService.Statistics("办公区域","入");
-    	JSONArray arr = new JSONArray();
-    	
-    	if(mapList == null || mapList.size() == 0){
-    		return R.error("获取失败");
-    	}
-    	
-    	for (Map<String, String> item : mapList) {
-    		arr.add(item);
-		}
-    	
-    	List<Map<String,String>> list = fbDooreventsService.Statistics("办公区域","出");
-    	
-    	if(list == null){
-    		return R.error("获取失败");
-    	}else if(list.size() == 0){
-    		return R.ok().put("data", arr);
-    	}
-    	
-    	for (Map<String, String> item : list) {
-    		for(int i=0;i<arr.size();i++){
-    			JSONObject obj = arr.getJSONObject(i);
-    			if(item.get("userName").equals(obj.getString("userName"))){
-    				//出时间必须大于入时间
-    				DateUtil.compare_dateTime(item.get("eventTime"), obj.getString("eventTime"), 2);
-    				
-    				
-					arr.remove(i);
-    			}
-    		}
-		}
 //    	
-//    	JSONObject obj = new JSONObject();
-//    	obj.put("total", arr.size());
-//    	obj.put("list", arr);
+//    	List<Map<String,String>> mapList = fbDooreventsService.Statistics("办公区域","入");
+//    	JSONArray arr = new JSONArray();
+//    	
+//    	if(mapList == null || mapList.size() == 0){
+//    		return R.error("获取失败");
+//    	}
+//    	
+//    	for (Map<String, String> item : mapList) {
+//    		arr.add(item);
+//		}
+//    	
+//    	List<Map<String,String>> list = fbDooreventsService.Statistics("办公区域","出");
+//    	
+//    	if(list == null){
+//    		return R.error("获取失败");
+//    	}else if(list.size() == 0){
+//    		return R.ok().put("data", arr);
+//    	}
+//    	
+//    	for (Map<String, String> item : list) {
+//    		for(int i=0;i<arr.size();i++){
+//    			JSONObject obj = arr.getJSONObject(i);
+//    			if(item.get("userName").equals(obj.getString("userName"))){
+//    				//出时间必须大于入时间
+//    				DateUtil.compare_dateTime(item.get("eventTime"), obj.getString("eventTime"), 2);
+//    				
+//    				
+//					arr.remove(i);
+//    			}
+//    		}
+//		}
+////    	
+////    	JSONObject obj = new JSONObject();
+////    	obj.put("total", arr.size());
+////    	obj.put("list", arr);
     	
     	return R.ok();
     }
     
     
-    /**
-     * 统计办公区域承包商人数
-     * <p>Title: getUser</p>
-     * <p>Description: 用户</p>
-     *
-     * @param params
-     * @return
-     * @author
-     */
-    @PassToken
-    @ResponseBody
-    @RequestMapping(value = "/StatisticsBGCBS", produces = "application/json;charset=utf-8", method = RequestMethod.POST)
-    public R StatisticsBGCBS() {
-    	
-    	List<Map<String,String>> mapList = fbDooreventsService.StatisticsCBS("办公区域");
-    	JSONArray arr = new JSONArray();
-    	
-    	for (Map<String, String> item : mapList) {
-    		if(item.get("menJinGN").equals("入")){
-    			arr.add(item);
-    		}
-		}
-    	
-    	for (Map<String, String> item : mapList) {
-    		if(item.get("menJinGN").equals("出")){
-    			for(int i = 0;i<arr.size();i++){
-    				JSONObject obj = arr.getJSONObject(i);
-    				if(item.get("userName").equals(obj.getString("userName"))){
-    					arr.remove(i);
-        			}
-    			}
-    		}
-		}
-    	
-    	JSONObject obj = new JSONObject();
-    	obj.put("total", arr.size());
-    	obj.put("list", arr);
-    	
-    	return R.ok().put("data",obj);
-    }
-    
-    
-    /**
-     * 统计生产区域人数（含码头）
-     * <p>Title: getUser</p>
-     * <p>Description: 用户</p>
-     *
-     * @param params
-     * @return
-     * @author
-     */
-    @PassToken
-    @ResponseBody
-    @RequestMapping(value = "/StatisticsSC", produces = "application/json;charset=utf-8", method = RequestMethod.POST)
-    public R StatisticsSC() {
-    	
-    	List<Map<String,String>> mapList = fbDooreventsService.Statistics("生产区域（含码头）","入");
-    	JSONArray arr = new JSONArray();
-    	
-    	for (Map<String, String> item : mapList) {
-    		if(item.get("menJinGN").equals("入")){
-    			arr.add(item);
-    		}
-		}
-    	
-    	for (Map<String, String> item : mapList) {
-    		if(item.get("menJinGN").equals("出")){
-    			for(int i = 0;i<arr.size();i++){
-    				JSONObject obj = arr.getJSONObject(i);
-    				if(item.get("userName").equals(obj.getString("userName"))){
-    					arr.remove(i);
-        			}
-    			}
-    		}
-		}
-    	
-    	JSONObject obj = new JSONObject();
-    	obj.put("total", arr.size());
-    	obj.put("list", arr);
-    	
-    	return R.ok().put("data",obj);
-    }
-    
-
-
    
 }
